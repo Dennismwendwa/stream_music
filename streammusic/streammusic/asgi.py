4447384ef35567739ae3fe69d9ba7efc3229dfb0
Dennismwendwa <dennismusembi2@gmail.com>
2023-08-28 05:03:54 +0300

    started new project. music streaming application

---
+++ streammusic/streammusic/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for streammusic project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'streammusic.settings')
+
+application = get_asgi_application()
